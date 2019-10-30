787d25480e9da3400e232e89402e9e53f3a65730
edward0rtiz <931@holbertonschool.com>
2019-10-29 19:02:07 -0500

    update sort with coktail prototypes

--- sort.h
+++ sort.h
@@ -33,7 +33,12 @@ void print_list(const listint_t *list);
 void swap_bubble(int *a, int *b);
 int partition(int *array, size_t size, int low, int high);
 void partition_sort(int *array, size_t size, int low, int high);
-
+void merge_sort_call(int *subarray, int *buffer, size_t lo, size_t hi);
+void merge_sub(int *subarray, int *buffer, size_t lo, size_t mid, size_t hi);
+void partition_sort_hoare(int *array, size_t size, int low, int high);
+int partition_hoare(int *array, size_t size, int low, int high);
+void swap_tail(listint_t **list, listint_t **head, listint_t **tail);
+void swap_head(listint_t **list, listint_t **tail, listint_t **head);
 
 /* algorithm functions */
 
@@ -41,5 +46,11 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+
+
 
 #endif
